adbb8ae1c6a915de7b6c569b35d1f79c24f23db3
Taras Glek <taras@glek.net>
2019-12-23 01:05:21 +0000

    sshuttle works too now

--- control.py
+++ control.py
@@ -1,7 +1,10 @@
 from subprocess import check_output, call
+import json
+import sys
 # https://gist.github.com/me7/12ad7c823a1b5d1a061dceb4fcd72b87
 # https://stackoverflow.com/questions/18924968/using-adb-to-access-a-particular-ui-control-on-the-screen
 def run(cmd):
+    print(cmd, file=sys.stderr)
     return check_output(cmd, shell=True).decode("utf-8")
 
 def ensure_screen_on():
@@ -13,9 +16,28 @@ def ensure_screen_on():
     # print("\n".join(lines))
     # run("adb shell input keyevent 82")
 
-def main():
+def termux():
     ensure_screen_on()
     run("adb shell am start -n com.termux/.app.TermuxActivity")
+
+def main():
+    run("adb forward tcp:8022 tcp:8022")
+    termux()
+    # figure out local addresses to exclude from sshuttle
+    # TODO: wonder how ip route output looks like when there is no outside routing
+    output = run("ip route get 1.1.1.1").split("\n")[0].split(' ')
+    wwan = output[4]
+    # get extended details for wwan interface
+    wwan_details = [
+        interface
+            for interface in json.loads(run("ip -j -p  addr show " + wwan))
+                if interface.get('ifname', None) == wwan]
+    # only do ipv4
+    addr_info = [details for details in wwan_details[0]['addr_info'] if details['family'] == 'inet'][0]
+    # get netmask and local ip
+    ip = addr_info['local']
+    netmask = addr_info['prefixlen']
+    run(f"sshuttle -v -x {ip}/{netmask}  --python python2 -r localhost:8022 0/0")
 
 if __name__ == "__main__":
     main()
